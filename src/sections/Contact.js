0888eeb743663df341917cf837bb6cfaccbe5aef
leomacode <forza.leoma@gmail.com>
2022-03-07 10:30:56 +0100

    fix contact section Id

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -4,7 +4,7 @@ import { SectionTitle } from "../components";
 
 function Contact() {
   return (
-    <section className="contact section-margin-top">
+    <section className="contact section-margin-top" id="contact">
       <SectionTitle title="contact" />
       <a href="mailto:forza.leoma@gmail.com">
         <span className="contact__btn--outline">Email me</span>
