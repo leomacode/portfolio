de558ce9e477794364a8f34c55e15e9c734d7898
leomacode <forza.leoma@gmail.com>
2022-03-22 10:45:03 +0100

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import { Navbar, Intro, Projects, Skills, Contact, Footer } from "./sections";
 import { BrowserRouter } from "react-router-dom";
+import ReactGa from "react-ga";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize("G-3MK0ZCFN9T");
+    // to report page view
+    ReactGa.pageview("/");
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="App">
