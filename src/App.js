3e665563f1d4c78f33585621882fc63a0f09bef4
leomacode <forza.leoma@gmail.com>
2022-03-22 14:20:08 +0100

    add Global site tag

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import "./App.css";
 import { Navbar, Intro, Projects, Skills, Contact, Footer } from "./sections";
 import { BrowserRouter } from "react-router-dom";
-import ReactGa from "react-ga";
 import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    ReactGa.initialize(`${process.env.REACT_APP_GOOGLE_ANALYTICS_API_KEY}`);
-    // to report page view
-    ReactGa.pageview("/");
-  }, []);
-
   return (
     <BrowserRouter>
       <div className="App">
